8391f49ca5009569c4c9c5575d6d865393383f39
Maxim Shestak <80178152+ms-matrp@users.noreply.github.com>
2026-07-28 00:43:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyl-HZbTTiksP_J0ij2-w7H9DmPmy-Fei2X-RIWLsPT8uaI-Q-c2Ls35J0eXUvYcsaNCQ/exec";
+const SCRIPT_URL = "const";
 
 // Авторизация
 const authSection = document.getElementById('authSection');
